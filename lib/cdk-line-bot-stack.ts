b213931aca235edd09c98be739181c4130922811
ufoo68 <mitarou.yuutarou@gmail.com>
2020-06-21 20:57:58 +0900

    For handson

--- lib/cdk-line-bot-stack.ts
+++ lib/cdk-line-bot-stack.ts
@@ -7,43 +7,7 @@ export class CdkLineBotStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props)
 
-    const table = new dynamodb.Table(this, 'Table', {
-      partitionKey: { name: 'userId', type: dynamodb.AttributeType.STRING }
-    })
-
-    const layer = new lambda.LayerVersion(this, 'layer', {
-      compatibleRuntimes: [lambda.Runtime.NODEJS_12_X],
-      code: lambda.Code.fromAsset('layer.out'),
-    })
-
-    const dbHandler = new lambda.Function(this, 'DbHandlerFunction', {
-      runtime: lambda.Runtime.NODEJS_12_X,
-      handler: 'index.handler',
-      code: lambda.Code.fromAsset('lambda/dbHandler'),
-      layers: [layer],
-      environment: {
-        TABLE_NAME: table.tableName,
-      }
-    })
-
-    const linebot = new lambda.Function(this, 'LineBotFunction', {
-      runtime: lambda.Runtime.NODEJS_12_X,
-      handler: 'index.handler',
-      code: lambda.Code.fromAsset('lambda/linebot'),
-      layers: [layer],
-      environment: {
-        ACCESS_TOKEN: process.env.ACCESS_TOKEN!,
-        CHANNEL_SECRET: process.env.CHANNEL_SECRET!,
-        FUNCTION_NAME: dbHandler.functionName,
-      }
-    })
-
-    dbHandler.grantInvoke(linebot)
-
-    table.grantFullAccess(dbHandler) 
-
-    const api = new apigateway.RestApi(this, 'Api')
-    api.root.addMethod('POST', new apigateway.LambdaIntegration(linebot))
+    // Implement cdk
 
   }
 }
